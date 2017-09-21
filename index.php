df772d4f72c5d4a59a5f153b35a8acef9d566aee
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-21 00:03:01 -0300

    testando POST e VARIAVEIS GLOBAIS do PHP

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<form action="http://127.0.0.1/Learning/GitPHP/login.php" method="POST">
+    <label>Login</label> <input type="text" name="login"/>
+    <label>Senha</label> <input type="password" name="senha"/>
+    <input type="submit"/>
+
+</form>
+
+<?php
+?>
+</body>
+
+</html>
---
+++ login.php
@@ -0,0 +1,32 @@
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css"/>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<?php
+echo '<pre>';
+var_dump($_POST);
+print_r($_POST);
+var_dump($_SERVER);
+print_r($_SERVER);
+echo $_SERVER["REMOTE_ADDR"];
+$nome = array(
+  "REMOTE_ADDR" => '127.0.0.1'
+);
+echo "</br>";
+echo $nome["REMOTE_ADDR"];
+echo "</br>";
+date_default_timezone_set('America/Sao_Paulo');
+$dataLocal = date('d/m/Y H:i:s', time());
+echo $dataLocal;
+echo "</br>";
+echo date("Y-m-d\TH:i:s\Z", $_SERVER["REQUEST_TIME"]);
+echo '</pre>';
+?>
+</body>
+</html>
