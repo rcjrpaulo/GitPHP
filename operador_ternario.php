f769aab9869316cd547714a827e29514191e50dd
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-21 19:29:13 -0300

    teste logico de PHP para atribuição

---
+++ operador_ternario.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<?php
+$n1 = $_GET["n1"];
+$n2 = $_GET["n2"];
+//teste logico pegando o maior numero, substituindo um if já fazendo atribuição.
+$maior = ($n1 > $n2) ? $n1 : $n2;
+echo "$maior";
+?>
+</body>
+</html>
