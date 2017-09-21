067118d6485d3bc8a480d03c1a6251ee5ff67c48
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-21 12:38:09 -0300

    teste de string

---
+++ ascii&stringleght.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<?php
+
+$nome = "Paulo";
+
+echo 'O nome da variavel $nome e '. $nome ."<br/>";
+echo "O valor da palavra pela tabela ascii e ". ord($nome)."<br/>" ;
+echo "Tamanho da String e " . strlen($nome) ."<br/>";
+
+
+
+
+?>
+</body>
+</html>
