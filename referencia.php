2c6b1fe7482b82b3e22921478b7fb848e77166bc
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-21 19:18:57 -0300

    ---

---
+++ referencia.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<?php
+$a = 5;
+$b = $a;
+$b +=10;
+echo "a = $a e b =$b";
+$c = &$a;
+$c +=5;
+//#c recebe mesmo endereço de memória de $a e quando altera o valor de c, altera do a também.
+echo "<br/>a = $a b =  $b c= $c";
+
+
+?>
+</body>
+</html>
