3febdfe0c838cb84e70f0b63310eaee8aea3d461
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-20 22:50:20 -0300

    commitando resto:

---
+++ operadores.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<?php
+$n1 = $_GET["a"];
+$n2 = $_GET["b"];
+echo "O primeiro numero vale $n1 e o segundo numero vale $n2 ";
+echo "<br/>A soma vale ". ($n1+$n2);
+echo "<br/>A subtração vale ". ($n1-$n2);
+echo "<br/>A multiplicacao vale ". ($n1*$n2);
+echo "<br/>A divisao vale ". ($n1/$n2);
+echo "<br/>O modulo vale ". ($n1%$n2);
+echo "<br/>A media vale ". (($n1+$n2)/2);
+
+?>
+</html>
---
+++ usandovariavel.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<?php
+$idade = 25;
+$nome = "Paulo";
+echo "$nome tem $idade anos";
+
+?>
+</html>
