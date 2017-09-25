175dff78455f14228924248f2e83ec43148fecad
rcjrpaulo <rcjrpaulo@gmail.com>
2017-09-25 12:03:19 -0300

    testandoifelse

---
+++ estruturarepeticao.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8"/>
+    <title>Ola Mundo</title>
+</head>
+<body>
+<?php
+
+$n1 = 1;
+$n2 = 2;
+$n3 = 3;
+
+if ($n1>$n2){
+    echo "O valor de $n1 e maior que $n2<br/>";
+}else{
+    echo "O valor de $n2 e maior que $n1<br/>";
+}
+if ($n2>$n1){
+    echo "O valor de $n2 e maior que $n1<br/>";
+}else{
+    echo "O valor de $n1 e maior que $n2<br/>";
+}
+if ($n2>$n3){
+    echo "O valor de $n2 e maior que $n3<br/>";
+}else{
+    echo "O valor de $n3 e maior que $n2<br/>";
+}
+if ($n3>$n2){
+    echo "O valor de $n3 e maior que $n2<br/>";
+}else{
+    echo "O valor de $n2 e maior que $n3<br/>";
+}
+if ($n1>$n3){
+    echo "O valor de $n1 e maior que $n3<br/>";
+}else{
+    echo "O valor de $n3 e maior que $n1<br/>";
+}
+if ($n3>$n1){
+    echo "O valor de $n3 e maior que $n1<br/>";
+}else{
+    echo "O valor de $n1 e maior que $n3<br/>";
+}
+
+
+
+
+?>
+</body>
+</html>
